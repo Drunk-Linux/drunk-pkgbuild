bf9c3de5a8399fdf03a4e2fc306040665bd5a3b3
Hilledkinged <kailing2@live.com>
2021-12-06 17:45:38 +0000

    Changes here and there...

--- cleanup.sh
+++ cleanup.sh
@@ -5,13 +5,13 @@ set -e
 echo " "
 echo "[*]: Cleaning up core"
 cd core
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*/
+rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/
 cd ..
 
 echo " "
 echo "[*]: Cleaning up extra"
 cd extra
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*/
+rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/
 cd ..
 
 echo " "
