83b3eeeb798b13c8ba5d64d2a2c26bfedaee8f91
Martin Valba <kailing2@live.com>
2022-05-10 20:20:50 +0300

    Some script side changes

--- x86_64/cleanup.sh
+++ x86_64/cleanup.sh
@@ -1,58 +1,43 @@
 #!/bin/bash
 set +e
 
+remove_leftovers() {
+    cd $@
+
+    echo " "
+    echo "[*]: Cleaning up $1"
+
+    rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
+
+    cd ..
+}
+
 echo " "
 echo "[*]: Moving pkg's to pkg dir"
 echo " "
 mv */*/*pkg.t* ../../pkgs/
 
 set -e
-echo " "
-echo "[*]: Cleaning up core"
-cd core
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
-cd ..
 
-echo " "
-echo "[*]: Cleaning up extra"
-cd extra
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
-cd ..
-
-echo " "
-echo "[*]: Cleaning up extra32"
-cd extra32
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
-cd ..
-
-echo " "
-echo "[*]: Cleaning up linux-modules"
-cd linux-modules
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
-cd ..
-
-echo " "
-echo "[*]: Cleaning up games"
-cd games
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
-cd ..
-
-echo " "
-echo "[*]: Cleaning up multilib"
-cd multilib
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
-cd ..
+remove_leftovers core
+remove_leftovers extra
+remove_leftovers extra32
+remove_leftovers linux-modules
+remove_leftovers games
+remove_leftovers multilib
 
 echo " "
 echo "[*]: Cleaning up desktop/*"
 cd desktop
 mv */*/*pkg.t* ../../../pkgs/
-cd xfce
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
-cd ../kde
-rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
+
+remove_leftovers xfce
+cd ..
+
+remove_leftovers kde
 cd ../..
 
+remove_leftovers server
 
 echo " "
 echo "[*]: Done"
