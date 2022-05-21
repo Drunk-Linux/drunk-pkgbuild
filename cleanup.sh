2d04f5f4aeaddd646223e89e71d0606b7e678028
Martin Valba <kailing2@live.com>
2022-05-21 21:08:58 +0300

    [ DRUNK ]: Changes here and there...

--- cleanup.sh
+++ cleanup.sh
@@ -4,13 +4,3 @@ echo "[!]: Cleaning up x86_64"
 cd x86_64
 ./cleanup.sh
 cd ..
-
-echo "[!]: Cleaning up i686"
-cd i686
-./cleanup.sh
-cd ..
-
-echo "[!]: Cleaning up aarch64"
-cd aarch64
-./cleanup.sh
-cd ..
--- x86_64/cleanup.sh
+++ x86_64/cleanup.sh
@@ -1,43 +1,59 @@
 #!/bin/bash
-set +e
 
 remove_leftovers() {
+    iam=$1
+
     cd $@
 
+    set +e
     echo " "
-    echo "[*]: Cleaning up $1"
+    echo "[*]: Moving $iam pkgs to proper place"
+    mv */*pkg.t* ../../pkgs/$iam/
+    set -e
+    echo "[*]: Cleaning up $iam"
 
     rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
 
     cd ..
 }
 
-echo " "
-echo "[*]: Moving pkg's to pkg dir"
-echo " "
-mv */*/*pkg.t* ../../pkgs/
-
 set -e
 
 remove_leftovers core
+remove_leftovers cross-tools
 remove_leftovers extra
 remove_leftovers extra32
-remove_leftovers linux-modules
 remove_leftovers games
-remove_leftovers multilib
-
-echo " "
-echo "[*]: Cleaning up desktop/*"
-cd desktop
-mv */*/*pkg.t* ../../../pkgs/
-
-remove_leftovers xfce
-cd ..
-
-remove_leftovers kde
-cd ../..
-
+remove_leftovers layers
+remove_leftovers proprietary
 remove_leftovers server
+
+remove_leftovers_desktop() {
+    iam=$1
+
+    cd $@
+
+    set +e
+    echo " "
+    echo "[*]: Moving desktop/$iam pkgs to proper place"
+    mv */*pkg.t* ../../../pkgs/desktop/$iam/
+    set -e
+
+    echo " "
+    echo "[*]: Cleaning up $iam"
+
+    rm -rf */pkg/ */src/ */*pkg* */*xz* */*tar.gz */*tar.bz2 */*.zip */*/ */*tgz */*tar.zst */*sign* */*sig* */*asc*
+
+    cd ..
+}
+
+cd desktop
+
+remove_leftovers_desktop xfce
+
+remove_leftovers_desktop kde
+
+remove_leftovers_desktop server
 
 echo " "
 echo "[*]: Done"
